9e84d5d657ebc311844e93f4ff31243d20017743
zhanqun <zhanqun@eccom.com.cn>
2026-07-12 07:56:34 +0800

    添加项目文件。

---
+++ MinioSync/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinioSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
